95668acc09e3425f79b4915f078d8ca1c331d1be
zkerriga <daniil.63@yandex.ru>
2020-09-14 19:20:08 +0300

    Add ft_tabdup and gitignore

---
+++ memory/ft_tabdup.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tabdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zkerriga <zkerriga@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/09/14 19:15:47 by zkerriga          #+#    #+#             */
+/*   Updated: 2020/09/14 19:15:49 by zkerriga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** The function creates a copy of a two-dimensional array of strings that
+** ends in NULL. The function is protected from leaks.
+*/
+
+char	**ft_tabdub(char **tab)
+{
+	char	**new_tab;
+	char	**tab_end;
+	int		i;
+
+	tab_end = tab;
+	while (tab_end)
+		++tab_end;
+	if ((new_tab = ft_calloc(sizeof(char *), (i = tab_end - tab))))
+	{
+		new_tab[i] = NULL;
+		while (--i)
+		{
+			if (!(new_tab[i] = ft_strdup(tab[i])))
+			{
+				while (new_tab[i])
+					free(new_tab[i++]);
+				free(new_tab);
+				new_tab = NULL;
+				break ;
+			}
+		}
+	}
+	return (new_tab);
+}
